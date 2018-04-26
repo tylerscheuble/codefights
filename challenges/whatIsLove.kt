0258986ddd8be894adec774cea394cc8a13bc72d
Tyler Scheuble <tyler@scheuble.us>
2018-04-25 17:00:29 -0700

    Remove some characters

--- challenges/whatIsLove.kt
+++ challenges/whatIsLove.kt
@@ -1,4 +1,4 @@
 /**
  * https://codefights.com/challenge/fH7stPgyynEgnePNy/
  */
-fun whatIsLove(n:Int):String="what is love baby don't hurt me don't hurt me no more".split(' ')[(n-1).rem(12)]
+fun whatIsLove(n:Int)="what is love baby don't hurt me don't hurt me no more".split(' ')[(n-1)%12]
